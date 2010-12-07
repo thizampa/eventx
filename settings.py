2c4a9680e3601f84ca8a633c988f66b405a966cd
Thiago <zampavix2@gmail.com>
2010-12-07 11:51:03 -0200

    alteração settings

--- settings.py
+++ settings.py
@@ -57,9 +57,9 @@ SECRET_KEY = 'a!v&m&a!uf$q-3ecyy8-xv-qj=y0e311k&a^&+a+_h#35%u78='
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+	'django.template.loaders.filesystem.Loader',
+	'django.template.loaders.app_directories.Loader',
+	#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
